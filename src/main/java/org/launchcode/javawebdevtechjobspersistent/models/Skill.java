081eb9abc5144d6da800ce788ab5b8ae03dabe2e
Renee <lrenee.mo@sbcglobal.net>
2020-02-05 13:18:11 -0600

    back to reading

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -10,6 +13,9 @@ public class Skill extends AbstractEntity {
         @NotBlank(message = "Skill description is required.")
         @Size(min = 2, max = 255, message = "Skill description must be between 2 and 255 characters.")
         private String description;
+
+        @ManyToMany(mappedBy = "skills")
+        private List<Job> jobs = new ArrayList<>();
 
         public Skill (String description) {
             this.description = description;
